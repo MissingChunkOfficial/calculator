2c7cd1064e59dcf273862fef5e707941394bf295
trent <trent@TRENTS-PC>
2016-06-19 18:15:24 +0100

    Add project files.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = "0";
+            resultValue = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "0")
+                textBox_result.Clear();
+
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + button.Text;
+        }
+
+        private void textBox_result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operationPerformed = button.Text;
+            resultValue = Double.Parse(textBox_result.Text);
+            labelCurrentOperation.Text = resultValue + "  " + isOperationPerformed;
+            isOperationPerformed = true;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = "0";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            switch(operationPerformed)
+            {
+                case "+":
+                    textBox_result.Text = (resultValue + Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "-":
+                    textBox_result.Text = (resultValue - Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "X":
+                    textBox_result.Text = (resultValue * Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "÷":
+                    textBox_result.Text = (resultValue / Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
